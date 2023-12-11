50a0ddbe12f48e7bb6dfc0457ec96130b219659f
viniciusalves <vinicius.alves@apicbase.com>
2023-12-10 22:12:56 -0300

    feat: set terraform state to be managed locally

--- countries/infrastructure/terraform.tf
+++ countries/infrastructure/terraform.tf
@@ -1,9 +1,15 @@
 terraform {
-  backend "s3" {
-    bucket = "vcloud-terraform-state"
-    key    = "applications/countries-app"
-    region = "sa-east-1"
-    dynamodb_table = "terraform-state-lock"
+  # Even though a remote state is best practice, this would make it much harder
+  # for others to quickly setup the project and test it
+  # backend "s3" {
+  #   bucket = "vcloud-terraform-state"
+  #   key    = "applications/countries-app"
+  #   region = "sa-east-1"
+  #   dynamodb_table = "terraform-state-lock"
+  # }
+  
+  backend "local" {
+    path = "terraform.tfstate"
   }
 
   required_providers {
