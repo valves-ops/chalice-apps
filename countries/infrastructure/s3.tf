eeefc82ff81ff13cc0a0755c78b946c2d9aa0cdc
viniciusalves <vinicius.alves@apicbase.com>
2023-12-11 00:13:21 -0300

    feat: update pipeline to handle chalice deployed state management

--- countries/infrastructure/s3.tf
+++ countries/infrastructure/s3.tf
@@ -1,3 +1,10 @@
 resource "aws_s3_bucket" "codepipeline_artifact_store" {
   bucket = "chalice-app-pipeline-artifact-store"
 }
+
+resource "aws_s3_object" "chalice_deployed_state" {
+    bucket = "${aws_s3_bucket.codepipeline_artifact_store.id}"
+    acl    = "private"
+    key    = "chalice-deployed-state/"
+    source = "/dev/null"
+}
